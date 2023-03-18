b14006dc5aa8a8aa86752cbfa762e82a69cab718
Arvind-4 <lordarvindthegamer@gmail.com>
2023-03-18 21:55:25 +0530

    Updated: new env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,21 @@
-require("dotenv").config();
+require('dotenv').config()
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 export default {
+  dev: process.env.NODE_ENV !== 'production',
+  env: process.env.NODE_ENV,
   port: parseInt(process.env.PORT) || 8000,
+  cyclicDB: {
+    url: process.env.CYCLIC_URL,
+    dbUrl: process.env.CYCLIC_DB,
+    bucketName: process.env.CYCLIC_BUCKET_NAME,
+    appId: process.env.CYCLIC_APP_ID
+  },
   logs: {
-    level: process.env.LOG_LEVEL || "silly",
+    level: process.env.LOG_LEVEL || 'silly'
   },
   api: {
-    prefix: "/api",
-  },
-};
+    prefix: '/api'
+  }
+}
